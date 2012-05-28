a80da1b341b23bc638a49b936f8f349d6eaf63e5
TEC <TEC@TEC-PC>
2012-05-28 17:36:40 -0500

    ADD .bat

---
+++ src/rmi1/paso1.bat
@@ -0,0 +1,5 @@
+cd C:\Users\TEC\Documents\NetBeansProjects\RMI1
+javac *.java
+rmic ServidorHola
+start rmiregistry
+java ServidorHola
---
+++ src/rmi1/paso2.bat
@@ -0,0 +1,2 @@
+cd C:\Users\TEC\Documents\NetBeansProjects\RMI1
+java ClienteHola
